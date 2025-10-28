9668e23417904faf09045abc5a6fb675f71ac0f3
alevtinafedorko-svg <alevtinafedorko@gmail.com>
2025-10-28 19:24:01 +0200

    change

--- home.js
+++ home.js
@@ -11,5 +11,13 @@ seeMyCoachButton.addEventListener("click", () => {
   // якщо selectedTrainer дорівнює "trainer1", location = "coach/trainer1.html"
   // можна використати if-else або switch
 
+  if(selectedTrainer ==="fitness"){
+          location = "coach/about1.html";
+    } else if(selectedTrainer ==="crossfit"){
+      location = "coach/about2.html";
+    } else if(selectedTrainer ==="nutrition"){
+      location = "coach/about3.html";
+    }
+
   window.location.href = location;
 });
